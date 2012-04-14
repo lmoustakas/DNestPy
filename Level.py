6867a66410d29d1e05e6311636565dbfcdc846d5
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-14 09:00:34 -0700

    Created LevelSet class for nice access to lists of levels

--- Level.py
+++ Level.py
@@ -4,26 +4,53 @@ class Level:
 	"""
 	Defines a Nested Sampling level
 	"""
-	def __init__(self, logX=0.0, logL=[-np.inf, 0.0]):
+	def __init__(self, logX=0.0, logL=[-1E300, 0.0]):
 		"""
 		Construct a level. By default, logX = 0
-		and logL = [-np.inf, 0.0]. i.e. the prior.
+		and logL = [-1E300, 0.0]. i.e. the prior.
+		I use -1E300 for compatibility with the C++ version.
 		"""
 		self.logX, self.logL = logX, logL
-		self.tries = 0
 		self.accepts = 0
-		self.visits = 0
+		self.tries = 0
 		self.exceeds = 0
+		self.visits = 0
 
 	def __str__(self):
 		s = str(self.logX) + " " + str(self.logL[0]) + " " \
 			+ str(self.logL[1]) + " "\
-			+ str(self.tries) + " "\
-			+ str(self.accepts) + " " + str(self.visits) + " "\
-			+ str(self.exceeds) + " "
+			+ str(self.accepts) + " "\
+			+ str(self.tries) + " " + str(self.exceeds) + " "\
+			+ str(self.visits) + " "
 		return s
 
+class LevelSet:
+	"""
+	Defines a set of levels. Implemented as a list
+	"""
+	def __init__(self, filename=None):
+		"""
+		Optional: load from file `filename`
+		"""
+		self.levels = []
+
+		if filename != None:
+			f = open('levels.txt', 'r')
+			lines = f.readlines()
+			for l in lines:
+				stuff = l.split()
+				level = Level(logX=float(stuff[0])\
+				,logL=[float(stuff[1]), float(stuff[2])])
+				level.accepts = int(stuff[3])
+				level.tries = int(stuff[4])
+				level.exceeds = int(stuff[5])
+				level.visits = int(stuff[6])
+				self.levels.append(level)
+
+	def __str__(self):
+		return "".join([str(l) + '\n' for l in self.levels])
+
 if __name__ == '__main__':
-	level = Level()
-	print(level)
+	levels = LevelSet('levels.txt')
+	print(levels)
 
